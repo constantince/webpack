a666fbc5322c6e19f10df655a2129a2aa68bcccd
constantince <alberteinstein007@126.com>
2018-09-28 15:45:29 +0800

    add the HMR plugin

--- entry/src/index.js
+++ entry/src/index.js
@@ -1,4 +1,4 @@
 import css from './a.css';
 import sass from './b.scss';
 
-console.log('hello webpack! 000');
+console.log('hello webpack!000');
--- entry/webpack.config.js
+++ entry/webpack.config.js
@@ -1,6 +1,10 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+const isProd = process.env.NODE_ENV;
+console.log(process.env.NODE_ENV);
 module.exports = {
 	mode: "development",//development|production|none
 	entry: {
@@ -9,21 +13,27 @@ module.exports = {
 	},
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: '[name].js'
+		filename: isProd ? '[name].[hash].js' : '[name].js'
 	},
 	module: {
 		rules: [
 			{
-			 test: /\.(css|scss)$/,
-			 use: [{
-				loader: MiniCssExtractPlugin.loader,
-			}, 'css-loader', 'sass-loader']}, //按数组顺序编译 style-loader, css-loader
+			 test: /\.(c|sc)ss$/,
+			 use: [!isProd ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+			 }, //按数组顺序编译 style-loader, css-loader
 			
 		]
 	},
+	devServer: {
+		port: 9000,
+		hot: true
+	},
 	plugins: [
+		new CleanWebpackPlugin(['dist']),
+
 		new MiniCssExtractPlugin({//将css文件与js文件分离
-			filename: "[name].css" //打包文件名称
+			filename: "[name].css", //打包文件名称
+			disable: !isProd
 		}),
 
 		new HtmlWebpackPlugin({//自动打包html文件
@@ -31,7 +41,9 @@ module.exports = {
 			title: 'Hello Webpack!', //网页标题
 			template: './index.html', //模板文件，不指定会把原版html中内容清空掉
 			hash: true
-		})
+		}),
+		new webpack.NamedModulesPlugin(),
+		new webpack.HotModuleReplacementPlugin(),
 
 	]
 };
