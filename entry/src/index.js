9e7507562ad91896405e045c564b93ff8a540762
constantince <alberteinstein007@126.com>
2019-05-30 19:35:12 +0800

    delete index.js

--- entry/src/index.js
+++
@@ -1,9 +0,0 @@
-import css from './a.css';
-import sass from './b.scss';
-import React from 'react';
-import ReactDom from 'react-dom';
-import App from './App';
-
-ReactDom.render(<App />, document.getElementById('root'));
-
-console.log('hello webpack!000');
